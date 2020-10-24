a26b21e0c54d13429b5dd97bb0509bd8b0a5c2c1
Muhamed Akl <muhamd.umar.akl@gmail.com>
2020-10-24 02:12:56 +0200

    robofriends frontend

---
+++ src/Containers/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import './App.css'
+import Scroll from '../Components/Scroll';
+class  App extends React.Component{
+  constructor(){
+    super();
+    this.state={  
+      robots:[],
+      searchField:''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users=>this.setState({robots:users}))
+  }
+
+  onSearchChange = (event)=>{
+   this.setState({searchField : event.target.value})
+  }
+  render =() => {
+    const {robots,searchField} = this.state;
+    const filteredRobots = robots.filter((element)=> {return element.name.toLowerCase().includes(searchField.toLowerCase())});
+
+    return !robots.length? <h1>Loading</h1>:
+    (
+    <div>
+        <h1 className="tc">Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots}/>
+        </Scroll>
+      </div>
+      );
+
+
+} 
+
+      
+}
+
+export default App;
